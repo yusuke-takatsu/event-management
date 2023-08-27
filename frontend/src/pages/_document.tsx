b98a79eca4ac892f93156c7bf6ff308ab10d48a5
github VinousTy <github ys.taka44@gmail.com>
2023-08-27 21:30:49 +0900

    feat:add meta tag settings

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -2,8 +2,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="ja">
+      <Head>
+        <meta name="form-detection" content="telephone=no" />
+        <meta name="robots" content="noindex,nofollow" />
+      </Head>
       <body>
         <Main />
         <NextScript />
