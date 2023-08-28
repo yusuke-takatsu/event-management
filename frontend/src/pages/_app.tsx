108638653d155306e7279635b260c4c78e209405
github VinousTy <github ys.taka44@gmail.com>
2023-08-29 07:33:40 +0900

    feat:add SWR

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,15 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { SWRConfig } from 'swr'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const swrConfigValue = {
+    fetcher: (url: string) => fetch(`${url}`).then((res) => res.json()),
+  }
+
+  return (
+    <SWRConfig value={swrConfigValue}>
+      <Component {...pageProps} />
+    </SWRConfig>
+  )
 }
