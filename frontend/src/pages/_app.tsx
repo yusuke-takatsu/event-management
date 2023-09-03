77af48f155431800f2d3df12c2b3743e9376e245
github VinousTy <github ys.taka44@gmail.com>
2023-09-03 14:30:21 +0900

    feat:add component settings using mui

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SWRConfig } from 'swr'
+import { ThemeProvider as MUIThemeProvider } from '@mui/material/styles'
+import { theme } from '../theme'
 
 export default function App({ Component, pageProps }: AppProps) {
   const swrConfigValue = {
@@ -8,8 +10,10 @@ export default function App({ Component, pageProps }: AppProps) {
   }
 
   return (
-    <SWRConfig value={swrConfigValue}>
-      <Component {...pageProps} />
-    </SWRConfig>
+    <MUIThemeProvider theme={theme}>
+      <SWRConfig value={swrConfigValue}>
+        <Component {...pageProps} />
+      </SWRConfig>
+    </MUIThemeProvider>
   )
 }
