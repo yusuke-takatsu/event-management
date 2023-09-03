03e07b06f86d317ff754426e4028b6b69c42ef42
github VinousTy <github ys.taka44@gmail.com>
2023-09-03 14:58:39 +0900

    change:emotion/cache and theme directories

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,18 +2,30 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SWRConfig } from 'swr'
 import { ThemeProvider as MUIThemeProvider } from '@mui/material/styles'
-import { theme } from '../theme'
+import { theme, createEmotionCache } from '../utils'
+import { EmotionCache } from '@emotion/cache'
+import { CacheProvider } from '@emotion/react'
 
-export default function App({ Component, pageProps }: AppProps) {
+const clientSideEmotionCache = createEmotionCache()
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache
+}
+
+export default function App(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
+
   const swrConfigValue = {
     fetcher: (url: string) => fetch(`${url}`).then((res) => res.json()),
   }
 
   return (
-    <MUIThemeProvider theme={theme}>
-      <SWRConfig value={swrConfigValue}>
-        <Component {...pageProps} />
-      </SWRConfig>
-    </MUIThemeProvider>
+    <CacheProvider value={emotionCache}>
+      <MUIThemeProvider theme={theme}>
+        <SWRConfig value={swrConfigValue}>
+          <Component {...pageProps} />
+        </SWRConfig>
+      </MUIThemeProvider>
+    </CacheProvider>
   )
 }
---
+++ frontend/src/utils/index.ts
@@ -0,0 +1,2 @@
+export { theme } from './muiThemeClient'
+export { createEmotionCache } from './createEmotionCache'
