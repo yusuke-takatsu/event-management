557bac4fa78fe2c2122f00aa105e997d5c311531
YusukeTakatsu <github ys.taka44@gmail.com>
2025-02-01 11:16:02 +0900

    feat:create logout

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,6 +18,6 @@ Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/register', [AuthController::class, 'register'])->name('register');
 Route::get('/verify/email/{token}', [AuthController::class, 'verifyEmail'])->name('verifyEmail');
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:user')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 });
