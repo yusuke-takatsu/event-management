20425678a6422df14f2bcfc0b5000b078dec47ed
YusukeTakatsu <github ys.taka44@gmail.com>
2025-02-01 11:10:23 +0900

    formatting with pint

--- app/Models/User.php
+++ app/Models/User.php
@@ -76,7 +76,7 @@ class User extends Authenticatable implements MustVerifyEmail
         return $this->email->value();
     }
 
-     /**
+    /**
      * {@inheritDoc}
      */
     public function markEmailAsVerified()
