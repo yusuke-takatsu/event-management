908ee0a75c57ef551eedcb3020239e0226ff2998
YusukeTakatsu <github ys.taka44@gmail.com>
2025-02-22 15:41:18 +0900

    fix:project name

--- .docker/local/mysql/mysql-docker-entrypoint.sql
+++ .docker/local/mysql/mysql-docker-entrypoint.sql
@@ -1,3 +1,3 @@
-CREATE DATABASE IF NOT EXISTS fishing_api_server_test;
-GRANT ALL PRIVILEGES ON *.* TO 'fishing_api_server_test'@'%' WITH GRANT OPTION;
+CREATE DATABASE IF NOT EXISTS event_management_test;
+GRANT ALL PRIVILEGES ON *.* TO 'event_management'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
